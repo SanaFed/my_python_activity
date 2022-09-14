318758dd7deebf99574d0f23d44b68c9b80cbd92
SanaFed <108747358+SanaFed@users.noreply.github.com>
2022-09-14 21:20:06 +0100

    1.2.5 Practical activity
    Write your first line of code

---
+++ my_first_code.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# this is my first line of code
+
+print("My name is Oksana Fedorova")
+print("The date is 14th September 2022")
+
+
+# In[2]:
+
+
+# Write a program to analyse real estate data
+
+a, b, c, d, e = 45000, 23400, 67000, 34600, 12900
+
+
+# In[3]:
+
+
+# Total sales from December
+
+print(a + b + c + d + e)
+
+
+# In[4]:
+
+
+# Average sales price for December
+
+print((a + b + c + d + e)/5)
+
+
+# In[5]:
+
+
+# Property price for property C
+
+print(c - 6500)
+
